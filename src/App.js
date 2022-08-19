88c74b5dd6076736e196569db18adc123323d1ce
Armin Hasanpour <hasanpour.ch@gmail.com>
2022-08-20 00:57:34 +0430

    get and show quote. add icons. some cleanup.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import './App.css';
 
 function App() {
+  const [quote, setQuote] = useState({});
+
+  const getQuote = async () => {
+    try {
+      const response = await axios.get('https://api.quotable.io/random');
+      setQuote(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getQuote();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row id="quote-box">
+        <Col id="text">
+          <p>{quote.content}</p>
+        </Col>
+        <Col id="author">
+          <p>{quote.author}</p>
+        </Col>
+        <button id="new-quote" type="button" onClick={getQuote}>New Quote</button>
+        <a id="tweet-quote" href="twitter.com/intent/tweet" target="_blank">twitter</a>
+      </Row>
+    </Container>
   );
 }
 
