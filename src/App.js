92b1ae9f721df0468555af303547e73c9f0deb31
Armin Hasanpour <hasanpour.ch@gmail.com>
2022-08-23 00:58:49 +0430

    get and show quote. add twitter button. add animations.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import GithubCorner from 'react-github-corner';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faQuoteLeft, faQuoteRight } from '@fortawesome/free-solid-svg-icons';
 
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import './App.css';
 
-function App() {
-  const [quote, setQuote] = useState({});
+/**
+   * Use on background color
+   * @returns random color
+   */
+const randomColor = () => {
+  const colors = ['#f16966', '#00413d', '#3c415e', '#c81d11', '#2e3136', '#d99058', '#0067a5', '#701c1c'];
+  return colors[Math.floor(Math.random() * colors.length)];
+};
 
+export default function App() {
+  const [quote, setQuote] = useState({
+    content: '',
+    author: '',
+  });
+  const [bgColor, setBgColor] = useState(randomColor());
+  const [textOpacity, setTextOpacity] = useState(0);
+
+  /**
+   * Fetch a quote from API
+   */
   const getQuote = async () => {
+    setBgColor(randomColor());
+    setTextOpacity(0);
+
     try {
       const response = await axios.get('https://api.quotable.io/random');
       setQuote(response.data);
     } catch (error) {
-      console.error(error);
+      setQuote({
+        content: error,
+        author: 'error',
+      });
     }
+
+    setTextOpacity(1);
   };
 
   useEffect(() => {
@@ -23,19 +49,49 @@ function App() {
   }, []);
 
   return (
-    <Container>
-      <Row id="quote-box">
-        <Col id="text">
-          <p>{quote.content}</p>
-        </Col>
-        <Col id="author">
-          <p>{quote.author}</p>
-        </Col>
-        <button id="new-quote" type="button" onClick={getQuote}>New Quote</button>
-        <a id="tweet-quote" href="twitter.com/intent/tweet" target="_blank">twitter</a>
-      </Row>
-    </Container>
+    <div className="wrapper" style={{ backgroundColor: bgColor }}>
+      <div id="quote-box">
+        <div id="text">
+          <p className="quote-text" style={{ color: bgColor, opacity: textOpacity }}>
+            <FontAwesomeIcon icon={faQuoteLeft} />
+            {' '}
+            {quote.content}
+            {' '}
+            <FontAwesomeIcon icon={faQuoteRight} />
+          </p>
+        </div>
+
+        <div id="author">
+          <p className="quote-text" style={{ color: bgColor, opacity: textOpacity }}>
+            -
+            {' '}
+            {quote.author}
+          </p>
+        </div>
+
+        <div className="card-controls">
+          <a
+            id="tweet-quote"
+            href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(`“${quote.content}”\n-${quote.author}`)}`}
+            target="_blank"
+            title="Share this on Twitter!"
+            rel="noreferrer"
+            style={{ backgroundColor: bgColor }}
+          >
+            <FontAwesomeIcon icon={faTwitter} />
+          </a>
+          <button
+            id="new-quote"
+            type="button"
+            onClick={getQuote}
+            style={{ backgroundColor: bgColor }}
+          >
+            New Quote
+          </button>
+        </div>
+      </div>
+
+      <GithubCorner href="https://github.com/hasanpour/fcc-quote" />
+    </div>
   );
 }
-
-export default App;
